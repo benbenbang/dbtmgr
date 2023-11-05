a8ada3a9d5a9d03a2a2f1df3afae96a9b4ee52d3
benbenbang <bn@bitbrew.dev>
2023-11-05 22:30:37 +0100

    refactor(wip): changes in cmd/root.go go.mod go.sum internal/config/config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,37 @@
 package config
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
 var AWS_DEFAULT_REGION = "eu-west-1"
 var DBT_STATE_BUCKET string
 var DBT_STATE_KEY string
 var DBT_LOCK_KEY string
 var Version string
+
+func Initialize() {
+	// 1. From the current path (last priority, where the binary is executed)
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config") // no need to include file extension
+
+	// 2. From the home directory (second priority)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Unable to find home directory:", err)
+		return
+	}
+	viper.AddConfigPath(home)
+
+	// Attempt to read the config
+	err = viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		fmt.Printf("Fatal error config file: %s \n", err)
+	}
+
+	// 3. Read environment variables that match (highest priority)
+	viper.AutomaticEnv()
+}
