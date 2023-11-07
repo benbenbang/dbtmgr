f496a76d78bed92719ae626b564cce3a94e0bc19
benbenbang <bn@bitbrew.dev>
2023-11-07 13:10:04 +0100

    refactor(scripts): add richful color for verkit

--- scripts/cli.py
+++ scripts/cli.py
@@ -1,12 +1,15 @@
 import tomlkit
 from pathlib import Path
+from rich import print
 
 
 def verkit():
+    print("[cyan]ℹ️ generating [bold]version.info[/bold] file...[/cyan]")
     project_root = Path(__file__).parent.parent
     pyproject_toml = tomlkit.loads((project_root / "pyproject.toml").read_text())
     version = pyproject_toml.get("tool", {}).get("poetry", {}).get("version", "")
     Path(project_root / "version.info").write_text(version)
+    print(f"[green]🎉 successfully generated version.info file with version: [bold]{version}[/bold][/green]")
 
 
 if __name__ == "__main__":
